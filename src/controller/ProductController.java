80e6c138664f76274ed7af24e86ad26fa290f926
CH-alt-bit <10635831@ucn.dk>
2025-12-09 18:31:12 +0100

    product delen færdig, retter imorgen

--- src/controller/ProductController.java
+++ src/controller/ProductController.java
@@ -1,5 +1,18 @@
 package controller;
+import model.Product;
+import container.ProductContainer;
 
 public class ProductController {
 
+	public ProductController() {	
+		
+	}
+	public void createProduct(int productId, String name, String description, String category, String model, String barcode, String location, double price) {
+		Product newProduct = new Product(productId, name, description, category, model, barcode, location, price);
+		ProductContainer container = ProductContainer.getInstance();
+		container.addProduct(newProduct);
+	}
+	public Product findProductByBarcode(String barcode) {
+		return ProductContainer.getInstance().findProductByBarcade(barcode);
+	}
 }
--- src/model/Product.java
+++ src/model/Product.java
@@ -1,89 +1,90 @@
 package model;
 
 public class Product {
-private int productId;
-private String name;
-private String description;
-private String category;
-private String model;
-private String barcode;
-private String location;
-private double price;
+	private int productId;
+	private String name;
+	private String description;
+	private String category;
+	private String model;
+	private String barcode;
+	private String location;
+	private double price;
 
-public Product(int productId, String name, String description, String category, String model, String barcode, String location, double price) {
-	this.productId = productId;
-	this.name = name;
-	this.description = description;
-	this.category = category;
-	this.model = model;
-	this.barcode = barcode;
-	this.location = location;
-	this.price = price;
-	
-}
+	public Product(int productId, String name, String description, String category, String model, String barcode,
+			String location, double price) {
+		this.productId = productId;
+		this.name = name;
+		this.description = description;
+		this.category = category;
+		this.model = model;
+		this.barcode = barcode;
+		this.location = location;
+		this.price = price;
 
-public int getProductId() {
-	return productId;
-}
+	}
 
-public void setProductId(int productId) {
-	this.productId = productId;
-}
+	public int getProductId() {
+		return productId;
+	}
 
-public String getName() {
-	return name;
-}
+	public void setProductId(int productId) {
+		this.productId = productId;
+	}
 
-public void setName(String name) {
-	this.name = name;
-}
+	public String getName() {
+		return name;
+	}
 
-public String getDescription() {
-	return description;
-}
+	public void setName(String name) {
+		this.name = name;
+	}
 
-public void setDescription(String description) {
-	this.description = description;
-}
+	public String getDescription() {
+		return description;
+	}
 
-public String getCategory() {
-	return category;
-}
+	public void setDescription(String description) {
+		this.description = description;
+	}
 
-public void setCategory(String category) {
-	this.category = category;
-}
+	public String getCategory() {
+		return category;
+	}
 
-public String getModel() {
-	return model;
-}
+	public void setCategory(String category) {
+		this.category = category;
+	}
 
-public void setModel(String model) {
-	this.model = model;
-}
+	public String getModel() {
+		return model;
+	}
 
-public String getBarcode() {
-	return barcode;
-}
+	public void setModel(String model) {
+		this.model = model;
+	}
 
-public void setBarcode(String barcode) {
-	this.barcode = barcode;
-}
+	public String getBarcode() {
+		return barcode;
+	}
 
-public String getLocation() {
-	return location;
-}
+	public void setBarcode(String barcode) {
+		this.barcode = barcode;
+	}
 
-public void setLocation(String location) {
-	this.location = location;
-}
+	public String getLocation() {
+		return location;
+	}
 
-public double getPrice() {
-	return price;
-}
+	public void setLocation(String location) {
+		this.location = location;
+	}
 
-public void setPrice(double price) {
-	this.price = price;
-}
+	public double getPrice() {
+		return price;
+	}
+
+	public void setPrice(double price) {
+		this.price = price;
+	}
 
 }
