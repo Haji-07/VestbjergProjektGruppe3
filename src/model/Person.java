7a8daae8e176ec397e2a7d79d8c5bcb8574050d3
Marculle <10700490@ucn.dk>
2025-12-09 10:09:17 +0100

    Update Person.java

--- src/model/Person.java
+++ src/model/Person.java
@@ -1,5 +1,61 @@
 package model;
 
 public class Person {
+	
+	private String name;
+	private String address;
+	private String postalCode;
+	private String phoneNumber;
+	private String email;
+	
+	public Person(String name, String address, String postalCode, String phoneNumber, String email) {
+		super();
+		this.name = name;
+		this.address = address;
+		this.postalCode = postalCode;
+		this.phoneNumber = phoneNumber;
+		this.email = email;
+	}
+	
+	public String getName() {
+		return name;
+	}
+	
+	public void setName(String name) {
+		this.name = name;
+		
+	}
+	
+	public String getAddress() {
+		return address;
+	}
+	
+	public void setAddress(String address) {
+		this.address = address;
+	}
+	
+	public String getPostalCode() {
+		return postalCode;
+	}
+	
+	public void setPostalCode(String postalCode) {
+		this.postalCode = postalCode;
+	}
+	
+	public String getPhoneNumber() {
+		return phoneNumber;
+	}
+	
+	public void setPhoneNumber(String phoneNumber) {
+		this.phoneNumber = phoneNumber;
+	}
+	
+	public String getEmail() {
+		return email;
+	}
+	
+	public void setEmail(String email) {
+		this.email = email;
+	}
 
 }
