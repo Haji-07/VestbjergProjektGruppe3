f03d7e6824638235fa9b4b822223ac9ffb02f3ce
Marculle <10700490@ucn.dk>
2025-12-09 11:14:38 +0100

    Customer klasse
    Har lavet getters og setters for Customer klassen.

--- src/model/Customer.java
+++ src/model/Customer.java
@@ -2,4 +2,23 @@ package model;
 
 public class Customer {
 
+	private int CustomerId;
+	private Person person;
+	private Business business;
+	
+	public Person getPerson(){
+		return person;
+	}
+	
+	public void setPerson(Person person) {
+		this.person = person;
+	}
+	
+	public Business getBusiness() {
+		return business;
+	}
+	
+	public void setBusiness(Business business) {
+		this.business = business;
+	}
 }
