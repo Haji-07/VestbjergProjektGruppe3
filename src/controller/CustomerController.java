678593fc8495ddf3c2af57b1c6bcc7685a2c0f5a
Marculle <10700490@ucn.dk>
2025-12-09 22:29:55 +0100

    Hele customer controller
    Burde gerne være den færdige version

--- src/controller/CustomerController.java
+++ src/controller/CustomerController.java
@@ -1,5 +1,51 @@
 package controller;
 
-public class CustomerController {
+import container.CustomerContainer;
+import model.Customer;
+import model.Person;
+import model.Business;
 
+public class CustomerController {
+	
+	private CustomerContainer customerContainer;
+	private int nextCustomerId;
+	
+	public CustomerController(){
+		customerContainer = CustomerContainer.getInstance();
+		nextCustomerId = 1;
+	}
+	
+	private int generateCustomerId() {
+		return nextCustomerId++;
+	}
+
+	public Customer createPrivateCustomer(String name, String address, String postalCode, String phoneNumber, String email) {
+		int id = generateCustomerId();
+		Person person = new Person(name, address, postalCode, phoneNumber, email);
+		Customer customer = new Customer(id, person);
+		
+		customerContainer.addCustomer(customer);
+		
+		return customer;
+		}
+	
+public Customer createBusinessCustomer(String name, String address, String postalCode, String phoneNumber, String email, String cvrNumber, String companyName) {
+
+		int id = generateCustomerId();
+		Person person = new Person(name, address, postalCode, phoneNumber, email);
+		Customer customer = new Customer(id, person);
+
+		Business business = new Business(cvrNumber, companyName);
+		customer.setBusiness(business);
+
+		customerContainer.addCustomer(customer);
+
+return customer;
 }
+
+	
+	public Customer findCustomerById(int customerId) {
+		return customerContainer.findCustomerById(customerId);
+		}
+	}
+
