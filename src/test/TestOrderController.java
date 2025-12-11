92aa494d5d5a1d838d576bc41ab20e0a04ffc9c0
Haji-07 <10711369@ucn.dk>
2025-12-11 13:18:08 +0100

    har lavet test for orderController

--- src/test/TestOrderController.java
+++ src/test/TestOrderController.java
@@ -1,5 +1,52 @@
 package test;
 
+import controller.CustomerController;
+import controller.OrderController;
+import controller.ProductController;
+import model.Order;
+import model.Person;
+import model.Product;
+import model.Customer;
+import container.OrderContainer;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
 public class TestOrderController {
+
+	@Test
+	void testCreateOrder() {
+		OrderController oc = new OrderController();
+		oc.createOrder();
+		assertEquals(0.0, oc.calculateOrderTotal());
+	}
+
+	@Test
+	void testAddProduct() {
+		OrderController oc = new OrderController();
+		oc.createOrder();
+
+		Product p = new Product("123", "Hammer", "En tung hammer", "Værktøj", "ModelX", "BAR123", "Reol A1", 50.0);
+
+		new ProductController().addProduct(p);
+
+		oc.addProductToOrder("123", 2);
+		assertEquals(100.0, oc.calculateOrderTotal());
+	}
+
+	@Test
+	void testAddCustomer() {
+		OrderController oc = new OrderController();
+		oc.createOrder();
+
+		Person person = new Person("Joe", "Joebanana boulevard 1", "9000", "12233445", "BananaJoe@gmail.com");
+		Customer c = new Customer(1, person);
+
+		new CustomerController().addCustomer(c);
+
+		oc.addCustomerToOrder(1);
+		assertEquals(0.0, oc.calculateOrderTotal());
+	}
 
 }
