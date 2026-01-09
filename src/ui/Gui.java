5f0fcc08b3bbadad2abd617b34e607a813ee0f8e
Marculle <10700490@ucn.dk>
2026-01-09 12:45:58 +0100

    Update Gui.java

--- src/ui/Gui.java
+++ src/ui/Gui.java
@@ -33,8 +33,47 @@ public class Gui extends JFrame {
 	        contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 	        setContentPane(contentPane);
 
-	        JPanel panel = new JPanel(new GridBagLayout()); 
+	        GridBagLayout gbl_panel = new GridBagLayout();
+	        gbl_panel.rowWeights = new double[]{1.0};
+	        gbl_panel.columnWeights = new double[]{1.0};
+	        JPanel panel = new JPanel(gbl_panel); 
 	        contentPane.add(panel, BorderLayout.CENTER);   
+	        
+	        JPanel panel_1 = new JPanel();
+	        GridBagConstraints gbc_panel_1 = new GridBagConstraints();
+	        gbc_panel_1.fill = GridBagConstraints.BOTH;
+	        gbc_panel_1.gridx = 0;
+	        gbc_panel_1.gridy = 0;
+	        panel.add(panel_1, gbc_panel_1);
+	        GridBagLayout gbl_panel_1 = new GridBagLayout();
+	        gbl_panel_1.columnWidths = new int[]{0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
+	        gbl_panel_1.rowHeights = new int[]{0, 0, 0};
+	        gbl_panel_1.columnWeights = new double[]{0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, Double.MIN_VALUE};
+	        gbl_panel_1.rowWeights = new double[]{0.0, 0.0, Double.MIN_VALUE};
+	        panel_1.setLayout(gbl_panel_1);
+	        
+	        JLabel lblNewLabel = new JLabel("New label");
+	        GridBagConstraints gbc_lblNewLabel = new GridBagConstraints();
+	        gbc_lblNewLabel.gridwidth = 9;
+	        gbc_lblNewLabel.insets = new Insets(0, 0, 5, 5);
+	        gbc_lblNewLabel.gridx = 0;
+	        gbc_lblNewLabel.gridy = 0;
+	        panel_1.add(lblNewLabel, gbc_lblNewLabel);
+	        
+	        JButton btnNewButton = new JButton("New button");
+	        GridBagConstraints gbc_btnNewButton = new GridBagConstraints();
+	        gbc_btnNewButton.insets = new Insets(0, 0, 5, 5);
+	        gbc_btnNewButton.gridx = 11;
+	        gbc_btnNewButton.gridy = 0;
+	        panel_1.add(btnNewButton, gbc_btnNewButton);
+	        
+	        JLabel lblNewLabel_1 = new JLabel("New label");
+	        GridBagConstraints gbc_lblNewLabel_1 = new GridBagConstraints();
+	        gbc_lblNewLabel_1.gridwidth = 9;
+	        gbc_lblNewLabel_1.insets = new Insets(0, 0, 0, 5);
+	        gbc_lblNewLabel_1.gridx = 0;
+	        gbc_lblNewLabel_1.gridy = 1;
+	        panel_1.add(lblNewLabel_1, gbc_lblNewLabel_1);
 
 	}
 
